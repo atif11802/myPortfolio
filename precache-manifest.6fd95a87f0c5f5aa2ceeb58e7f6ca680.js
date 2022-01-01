c78c338822f93df52288bc949084b6c6d8f3466b
atif11802 <ratul0947@gmail.com>
2022-01-01 15:52:44 +0600

    Updates

---
+++ precache-manifest.6fd95a87f0c5f5aa2ceeb58e7f6ca680.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89cfb9534b3a20e60584630be07eb9ca",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "83e09df2b1fed7b57456",
+    "url": "/myPortfolio/static/css/main.2dd2430f.chunk.css"
+  },
+  {
+    "revision": "08af805f18306dafd588",
+    "url": "/myPortfolio/static/js/2.526a70dc.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/myPortfolio/static/js/2.526a70dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e09df2b1fed7b57456",
+    "url": "/myPortfolio/static/js/main.9c8e55be.chunk.js"
+  },
+  {
+    "revision": "0f713a185793586653f3",
+    "url": "/myPortfolio/static/js/runtime-main.d351083c.js"
+  },
+  {
+    "revision": "244edf940955e21723c0702a5dd1e1f2",
+    "url": "/myPortfolio/static/media/Mern Ecommerce.244edf94.png"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/myPortfolio/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/myPortfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "4d56554ca9edc707b3643416fff3591a",
+    "url": "/myPortfolio/static/media/project2.4d56554c.png"
+  },
+  {
+    "revision": "7614ce5e3920e48a56418be63ac1eba4",
+    "url": "/myPortfolio/static/media/project3.7614ce5e.png"
+  },
+  {
+    "revision": "3a1aca11621b7ced48e8e7475dfc77c3",
+    "url": "/myPortfolio/static/media/project4.3a1aca11.png"
+  },
+  {
+    "revision": "f4fa9523a06ed58a058c926a9dfdbfd6",
+    "url": "/myPortfolio/static/media/project5.f4fa9523.png"
+  },
+  {
+    "revision": "462a1a082eb606d47f701f91b5f7b285",
+    "url": "/myPortfolio/static/media/project6.462a1a08.png"
+  },
+  {
+    "revision": "5ff2d5f07b9aae7d3d7094d56a4405b6",
+    "url": "/myPortfolio/static/media/project7.5ff2d5f0.png"
+  },
+  {
+    "revision": "e55b95fceb8ba0ea995db7a9533b400f",
+    "url": "/myPortfolio/static/media/weather.e55b95fc.png"
+  }
+]);
