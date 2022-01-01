fdcb7429266431b9ed6d5ddea478103e4cff07d4
atif11802 <ratul0947@gmail.com>
2022-01-01 15:56:47 +0600

    updated root directory

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ import Contact from "./components/Contact";
 import "./App.css";
 
 function App() {
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/resume" component={Resume} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
-      </Switch>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<Navbar />
+			<Switch>
+				<Route exact path='/myPortfolio' component={Home} />
+				<Route exact path='/resume' component={Resume} />
+				<Route exact path='/portfolio' component={Portfolio} />
+				<Route exact path='/contact' component={Contact} />
+			</Switch>
+		</React.Fragment>
+	);
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,88 +23,88 @@ import avatar from "../avatar.png";
 import Footer from "../components/Footer";
 
 const useStyles = makeStyles((theme) => ({
-  appbar: {
-    background: "#222",
-    margin: 0,
-  },
-  arrow: {
-    color: "tomato",
-  },
-  title: {
-    color: "tan",
-  },
-  menuSliderContainer: {
-    width: 250,
-    background: "#511",
-    height: "100%",
-  },
-  avatar: {
-    display: "block",
-    margin: "0.5rem auto",
-    width: theme.spacing(13),
-    height: theme.spacing(13),
-  },
-  listItem: {
-    color: "tan",
-  },
+	appbar: {
+		background: "#222",
+		margin: 0,
+	},
+	arrow: {
+		color: "tomato",
+	},
+	title: {
+		color: "tan",
+	},
+	menuSliderContainer: {
+		width: 250,
+		background: "#511",
+		height: "100%",
+	},
+	avatar: {
+		display: "block",
+		margin: "0.5rem auto",
+		width: theme.spacing(13),
+		height: theme.spacing(13),
+	},
+	listItem: {
+		color: "tan",
+	},
 }));
 
 const menuItems = [
-  { listIcon: <Home />, listText: "Home", listPath: "/" },
-  { listIcon: <AssignmentInd />, listText: "Resume", listPath: "/resume" },
-  { listIcon: <Apps />, listText: "Portfolio", listPath: "/portfolio" },
-  { listIcon: <ContactMail />, listText: "Contact", listPath: "/contact" },
+	{ listIcon: <Home />, listText: "Home", listPath: "/myPortfolio" },
+	{ listIcon: <AssignmentInd />, listText: "Resume", listPath: "/resume" },
+	{ listIcon: <Apps />, listText: "Portfolio", listPath: "/portfolio" },
+	{ listIcon: <ContactMail />, listText: "Contact", listPath: "/contact" },
 ];
 
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const sideList = () => (
-    <Box className={classes.menuSliderContainer} component="div">
-      <Avatar className={classes.avatar} src={avatar} alt="Mahmudul Alam" />
-      <Divider />
-      <List>
-        {menuItems.map((item, i) => (
-          <ListItem
-            button
-            key={i}
-            className={classes.listItem}
-            onClick={() => setOpen(false)}
-            component={Link}
-            to={item.listPath}
-          >
-            <ListItemIcon className={classes.listItem}>
-              {item.listIcon}
-            </ListItemIcon>
-            <ListItemText primary={item.listText} />
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
+	const sideList = () => (
+		<Box className={classes.menuSliderContainer} component='div'>
+			<Avatar className={classes.avatar} src={avatar} alt='Mahmudul Alam' />
+			<Divider />
+			<List>
+				{menuItems.map((item, i) => (
+					<ListItem
+						button
+						key={i}
+						className={classes.listItem}
+						onClick={() => setOpen(false)}
+						component={Link}
+						to={item.listPath}
+					>
+						<ListItemIcon className={classes.listItem}>
+							{item.listIcon}
+						</ListItemIcon>
+						<ListItemText primary={item.listText} />
+					</ListItem>
+				))}
+			</List>
+		</Box>
+	);
 
-  return (
-    <React.Fragment>
-      <Box component="nav">
-        <AppBar position="static" className={classes.appbar}>
-          <Toolbar>
-            <IconButton onClick={() => setOpen(true)}>
-              <ArrowBack className={classes.arrow} />
-            </IconButton>
-            <Typography variant="h5" className={classes.title}>
-              Portfolio
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <Drawer open={open} anchor="right" onClose={() => setOpen(false)}>
-        {sideList()}
-        <Footer />
-      </Drawer>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Box component='nav'>
+				<AppBar position='static' className={classes.appbar}>
+					<Toolbar>
+						<IconButton onClick={() => setOpen(true)}>
+							<ArrowBack className={classes.arrow} />
+						</IconButton>
+						<Typography variant='h5' className={classes.title}>
+							Portfolio
+						</Typography>
+					</Toolbar>
+				</AppBar>
+			</Box>
+			<Drawer open={open} anchor='right' onClose={() => setOpen(false)}>
+				{sideList()}
+				<Footer />
+			</Drawer>
+		</React.Fragment>
+	);
 };
 
 export default Navbar;
